ca352f81405c9f2d47951a0e26b373af609a9b69
Gorthdzx <krislaksana123@gmail.com>
2024-01-05 20:20:01 +0700

    refactor: main activity

--- app/src/main/java/app/dito/tipcalculatorapp/MainActivity.kt
+++ app/src/main/java/app/dito/tipcalculatorapp/MainActivity.kt
@@ -1,13 +1,17 @@
 package app.dito.tipcalculatorapp
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import app.dito.tipcalculatorapp.components.TipTimeLayout
 import app.dito.tipcalculatorapp.ui.theme.TipCalculatorAppTheme
@@ -20,7 +24,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
                 ) {
                     TipCalculatorPreview()
                 }
@@ -31,10 +35,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TipCalculatorApp() {
-    TipTimeLayout()
+    TipTimeLayout(modifier = Modifier.background(color = Color.Gray))
 }
 
-@Preview(showBackground = true)
+/*
+SET DARK MODE
+ */
+@Preview(
+    name = "Dark Mode",
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_YES,
+)
+/*
+SET LIGHT MODE
+ */
+@Preview(
+    name = "Light Mode",
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_NO,
+)
+annotation class ModePreviews
+
+@ModePreviews
 @Composable
 fun TipCalculatorPreview() {
     TipCalculatorAppTheme {
